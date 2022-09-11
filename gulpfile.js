6bd27264388c6a7c2e5d96a11302d9b35ed165e4
maleja274 <maleja274@hotmail.com>
2022-09-10 23:10:30 -0500

    unificacion.css proyecto festival

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel} = require("gulp");
 //css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 
 //imagenes
@@ -13,8 +17,11 @@ const avif = require("gulp-avif");
 
 function css(done) {
     src("src/scss/**/*.scss")  // Identificar el archivo SASS
+     .pipe(sourcemaps.init())
      .pipe(plumber())
      .pipe(sass())  // Compilarlo
+     .pipe(postcss([autoprefixer(), cssnano()]) )
+     .pipe(sourcemaps.write('.'))
      .pipe(dest("build/css") ) //Almacenarla en el disco duro 
     done(); // Callback que avisa a gulp cuando llegamos al final 
 }
