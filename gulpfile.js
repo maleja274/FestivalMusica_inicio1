bc5e2643e128e7c501a897638e9ad5c558fddb02
maleja274 <maleja274@hotmail.com>
2022-09-12 19:49:57 -0500

    final-proyecto.festival-musica

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,9 @@ const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+//javascript
+const terser = require("gulp-terser-js");
+
 function css(done) {
     src("src/scss/**/*.scss")  // Identificar el archivo SASS
      .pipe(sourcemaps.init())
@@ -58,6 +61,9 @@ function versionAvif(done) {
 
 function javascript(done) {
     src("src/js/**/*.js")
+       . pipe(sourcemaps.init() )
+       .pipe(terser() )
+       .pipe(sourcemaps.write(".") )
        .pipe(dest("build/js"))
     done();
 }
